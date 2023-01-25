9c90997f2cb5f7c194d0a03812a57075b441ba1a
Teresa Fuckyouiamnotputingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-01-24 19:28:44 -0500

    Added Rust structs for vec*s
    Added Rust structs for vec*s, no methods however.

--- src/physics/vec.rs
+++ src/physics/vec.rs
@@ -21,4 +21,43 @@ pub mod ffi
     }
 }
 
-// TODO: Actual stuff
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct Vec2
+{
+    pub x: f32,
+    pub y: f32,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct Vec3
+{
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct Vec4
+{
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub w: f32,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct Quat
+{
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub w: f32,
+}
+
+type Quaternion = Quat;
+
+// TODO: impl's
