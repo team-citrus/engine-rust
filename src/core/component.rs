7889c71d4b920ba98e05f0d224313123dbb9b699
Teresa <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-03-07 15:24:45 +0000

    Changed the way Rust components are executed.
    Changed the way Rust components are executed... again.

--- src/core/component.rs
+++ src/core/component.rs
@@ -6,16 +6,21 @@
 *	license: LGPL-3.0-only
 */
 
-use crate::{mem::*, Vector};
+use crate::{mem::*, vector::Vector};
+use std::ptr::*;
+use impls::impls;
 
 #[allow(dead_code)]
+#[feature(ptr_as_ref)]
 
 // TODO: OPTIMIZE THE FUCK OUT OF THIS SHIT
-// TODO: Strip out the dark magic
+// TODO: Strip out the dark magic as best as possible
 
 type ComponentFuncPtr = fn (usize) -> (); // Dark magic, references are pointers and a usize is the same size as a pointer
 
 // Dark magic, all components are #[repr(C)]
+// This exists purely to enable execution directly from C++
+#[repr(C)]
 pub struct Base
 {
     awake_fn: ComponentFuncPtr,
@@ -29,40 +34,77 @@ pub struct Base
 
 pub trait Component
 {
-    // TODO: object and stuff
+
 }
 
 pub trait ComponentAwake
 {
-    fn awake(&self) -> ();
+    pub fn awake(&self) -> ();
 }
 
 pub trait ComponentStart
 {
-    fn start(&self) -> ();
+    pub fn start(&self) -> ();
 }
 
 pub trait ComponentUpdate
 {
-    fn update(&self) -> ();
+    pub fn update(&self) -> ();
 }
 
 pub trait ComponentFixedUpdate
 {
-    fn fixed_update(&self) -> ();
+    pub fn fixed_update(&self) -> ();
 }
 
 pub trait ComponentOnTriggers
 {
-    fn on_trigger_enter(&self) -> ();
-    fn on_trigger_stay(&self) -> ();
-    fn on_trigger_exit(&self) -> ();
+    pub fn on_trigger_enter(&self) -> ();
+    pub fn on_trigger_stay(&self) -> ();
+    pub fn on_trigger_exit(&self) -> ();
 }
 
-rust_components: Vector<usize>; // Vector of pointers to components, stored as integers for ease of access
-
-#[no_mangle]
-extern "C" fn rust_exec_gameplay() -> ()
+macro_rules! __load_base
 {
-    // TODO: stuff
+    ($x:ident: $type:ty) => {
+        let base: &mut Base
+
+        unsafe {
+           base = &$x.cast<Base>().cast_mut().as_ref();
+        }
+
+        *base.awake_fn = if impls!(T: ComponentAwake) {
+            x.awake
+        } else {
+            0 as ComponentFuncPtr
+        }
+
+        *base.start_fn = if impls!(T: ComponentStart) {
+            x.start
+        } else {
+            0 as ComponentFuncPtr
+        }
+
+        *base.update_fn = if impls!(T: ComponentUpdate) {
+            x.update
+        } else {
+            0 as ComponentFuncPtr
+        }
+
+        *base.fixed_update_fn = if impls!(T: ComponentFixedUpdate) {
+            x.fixed_update
+        } else {
+            0 as ComponentFuncPtr
+        }
+
+        if impls!(T: ComponentOnTriggers) {
+            *base.on_trigger_enter_fn = x.on_trigger_enter;
+            *base.on_trigger_stay_fn = x.on_trigger_stay;
+            *base.on_trigger_exit_fn = x.on_trigger_exit;
+        } else {
+            *base.on_trigger_enter_fn = 0 as ComponentFuncPtr;
+            *base.on_trigger_stay_fn = 0 as ComponentFuncPtr;
+            *base.on_trigger_exit_fn = 0 as ComponentFuncPtr;
+        }
+    }
 }
